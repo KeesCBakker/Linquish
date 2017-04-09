f5e59a3f5940d2d50bb428dbe3b2721ab922d9d6
Kees C. Bakker <c.bakker@gmx.net>
2017-04-09 14:24:01 +0200

    Pleasing npm.

--- test/test-bridge.js
+++ test/test-bridge.js
@@ -1,30 +1,33 @@
-﻿//build a bridge for exports and module objects
-exports = window.exports || {};
+﻿if (typeof module === 'undefined') {
 
-//build a bridge for require
-require = window.require || function (x) {
+    //build a bridge for exports and module objects
+    exports = window.exports || {};
 
-    //might be implemented as a global property
-    if (typeof window[x] !== 'undefined') {
-        console.log('Using window for: ' + x);
-        return window[x];
+    //build a bridge for require
+    require = window.require || function (x) {
+
+        //might be implemented as a global property
+        if (typeof window[x] !== 'undefined') {
+            console.log('Using window for: ' + x);
+            return window[x];
+        }
+
+        //check if there is a script that used exports
+        var tags = document.getElementsByTagName('script');
+        var scripts = Array.prototype.slice.call(tags);
+
+        var name = x;
+        name = name.replace(/(\.|\/)/gi, '');
+
+        var useExports = scripts.some(function (node) {
+            return node.getAttribute('data-export') == name;
+        });
+
+        if (useExports) {
+            console.log('Using exports for: ' + x);
+            return exports;
+        }
+
+        return null;
     }
-
-    //check if there is a script that used exports
-    var tags = document.getElementsByTagName('script');
-    var scripts = Array.prototype.slice.call(tags);
-
-    var name = x;
-    name = name.replace(/(\.|\/)/gi, '');
-
-    var useExports = scripts.some(function (node) {
-        return node.getAttribute('data-export') == name;
-    });
-
-    if (useExports) {
-        console.log('Using exports for: ' + x);
-        return exports;
-    }
-
-    return null;
 }
